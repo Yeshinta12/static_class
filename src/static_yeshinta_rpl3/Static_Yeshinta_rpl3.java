540d583035a3d94381f03cb3dcb5f11ed6410650
yesin <yesin@Yeshinta>
2024-05-29 15:41:59 +0700

    Upload Project

---
+++ src/static_yeshinta_rpl3/Static_Yeshinta_rpl3.java
@@ -0,0 +1,43 @@
+package static_yeshinta_rpl3;
+
+class Display{
+    static  String type = "Display";
+    private String name;
+
+    Display(String name){
+        this.name = name;
+    }
+
+    void setType(String typeInput){
+        // type = typeInput; //alternatif 1
+        // this.type = typeInput; //alternatif 2
+        Display.type = typeInput; //alternatif 3
+    }
+
+    void show(){
+        System.out.println("Display name = " + this.name);
+    }
+}
+
+
+public class Static_Yeshinta_rpl3 {
+
+    public static void main(String[] args) {
+         Display display1 = new Display("Monitor");
+        display1.show();
+
+        Display display2 = new Display("Smartphone");
+        display2.show();
+
+        // tampilkan static variable atau class variable
+
+        // kita coba mengganti variable staticnya
+        display1.setType("Monitor");
+
+        System.out.println("\nMenampilkan static variable");
+        System.out.println(display1.type);
+        System.out.println(display2.type);
+        System.out.println(Display.type);
+
+   }
+}
